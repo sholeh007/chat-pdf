3c9ab1b12a90d4234e227213997c19e99529a694
sholeh007 <sholeh09@outlook.com>
2023-10-11 15:12:07 +0900

    add feature section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={cn("min-h-screen antialiased font-sans", roboto.className)}>
+      <body className={cn("min-h-screen antialiased grainy", roboto.className)}>
         {children}
       </body>
     </html>
